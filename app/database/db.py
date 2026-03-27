d785bb92e3c9cdf825c37fe9a58134208cf5ffc0
Prashant Singh <prashants@CPU-006-IL.quidich.local>
2026-03-27 13:54:10 +0530

    fix postgres dict cursor

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,20 @@
 import psycopg2
+from psycopg2.extras import RealDictCursor
 from flask import g
 import os
 
 
 def get_db():
     if 'db' not in g:
-        g.db = psycopg2.connect(os.environ.get("DATABASE_URL"))
+        db_url = os.environ.get("DATABASE_URL")
+
+        if not db_url:
+            raise Exception("DATABASE_URL not set")
+
+        # 🔥 IMPORTANT CHANGE
+        g.db = psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+        g.db.autocommit = True
+
     return g.db
 
 
